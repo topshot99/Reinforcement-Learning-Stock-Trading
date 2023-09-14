2e7873d7b64c09486e3b471914b660a4cba4a7d3
Milan Agrawal <milanagrawal@microsoft.com>
2023-09-14 11:37:32 +0530

    Add a MultiData env

--- data-fetch.py
+++ data-fetch.py
@@ -1,9 +1,8 @@
 import yfinance as yf
 import os
-
 # Get the data for Hindustan Unilever from 2000 to 2015
-data = yf.download("HINDUNILVR.NS", start="2016-01-01", end="2018-12-31")
+data = yf.download("AAPL", start="2016-01-01", end="2018-12-31")
 
 # Save the data to a CSV file in the data folder
 
-data.to_csv(os.path.join("test_data", "HINDUNILVR.csv"))
+data.to_csv(os.path.join("data", "AAPL.csv"))
