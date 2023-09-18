78153a8b823274f60a8e693739fc7447a8f41e03
Milan Agrawal <milanagrawal@microsoft.com>
2023-09-18 13:49:01 +0530

    Add metrics to the render

--- renderscript.py
+++ renderscript.py
@@ -1,3 +1,12 @@
 from gym_trading_env.renderer import Renderer
+import pandas as pd
 renderer = Renderer(render_logs_dir="render_logs")
+renderer.add_metric(
+    name = "Annual Market Return",
+    function = lambda df : f"{ ((df['close'].iloc[-1] / df['close'].iloc[0])**(pd.Timedelta(days=365)/(df.index.values[-1] - df.index.values[0]))-1)*100:0.2f}%"
+)
+renderer.add_metric(
+        name = "Annual Portfolio Return",
+        function = lambda df : f"{((df['portfolio_valuation'].iloc[-1] / df['portfolio_valuation'].iloc[0])**(pd.Timedelta(days=365)/(df.index.values[-1] - df.index.values[0]))-1)*100:0.2f}%"
+)
 renderer.run()
